14d95da0eedbbcf995c0005e7568756000b2b46c
Mark Stevenson <M.Stevenson@massey.ac.nz>
2011-05-10 00:00:00 +0000

    version 0.9-32

--- R/epi.simplesize.R
+++ R/epi.simplesize.R
@@ -1,23 +1,26 @@
-epi.simplesize <- function(N = 1E+06, sd, Py, epsilon.a, method = "mean", conf.level = 0.95) 
+epi.simplesize <- function(N = 1E+06, Vsq, Py, epsilon.r, method = "mean", conf.level = 0.95) 
 {
     N. <- 1 - ((1 - conf.level) / 2)
     z <- qnorm(N., mean = 0, sd = 1)
     
     if (method == "total") {
-        n <- (z^2 * sd^2) / epsilon.a^2
+        # Page 74 Levy and Lemeshow (equation 3.14):
+        n <- (z^2 * N * Vsq) / (z^2 * Vsq + ((N - 1) * epsilon.r^2))
         f <- n / N
         if(f > 0.10){n <- n / (1 + n/N)}
         rval <- round(n, digits = 0)
     }
 
     if (method == "mean") {
-        n <- (z^2 * sd^2) / epsilon.a^2
+        # Page 74 Levy and Lemeshow (equation 3.15):
+        n <- (z^2 * N * Vsq) / (z^2 * Vsq + ((N - 1) * epsilon.r^2))
         f <- n / N
         if(f > 0.10){n <- n / (1 + n/N)}
         rval <- round(n, digits = 0)
     }
     if (method == "proportion") {
-        n <- (z^2 * (1 - Py) * Py) / (epsilon.a^2)
+        # Page 74 Levy and Lemeshow (equation 3.16):
+        n <- (z^2 * N * (1 - Py) * Py) / (((N - 1) * (epsilon.r^2) * Py^2) + (z^2 * Py * (1 - Py)))
         f <- n / N
         if(f > 0.10){n <- n / (1 + n/N)}
         rval <- round(n, digits = 0)
