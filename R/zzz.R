3efa83a425e327ec65c0587d1a398d631cff0b09
Mark Stevenson <M.Stevenson@massey.ac.nz>
2012-12-05 07:57:55 +0000

    version 0.9-45

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,7 @@
-.First.lib <- function(libname, pkgname)
+.onLoad <- function(libname, pkgname)
 {
-   ver <- read.dcf(file.path(libname, pkgname, "DESCRIPTION"), "Version")
-   ver <- as.character(ver)	
-   packageStartupMessage("Package epiR ", ver, " is loaded", appendLF = TRUE)
-   packageStartupMessage("Type help(epi.about) for summary information")
-   packageStartupMessage("\n")
+ver <- as.character(read.dcf(file.path(libname, pkgname, "DESCRIPTION"), "Version"))
+packageStartupMessage("Package epiR ", ver, " is loaded", appendLF = TRUE)
+packageStartupMessage("Type help(epi.about) for summary information")
+packageStartupMessage("\n")
 }
-
-
-
