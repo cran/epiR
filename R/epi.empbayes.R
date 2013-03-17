56f396c9dcfc7704f22970548b7c9665876f0e78
Mark Stevenson <M.Stevenson@massey.ac.nz>
2013-03-17 00:00:00 +0000

    version 0.9-46

--- R/epi.empbayes.R
+++ R/epi.empbayes.R
@@ -1,30 +1,29 @@
 "epi.empbayes" <- function(obs, pop){
 	# gamma: mean of rate
-        # phi: variance of rate
+  # phi: variance of rate
         
-        gamma <- (sum(obs))/(sum(pop))
-        rate <- obs/pop
-        sum.pop <- sum(pop)
+  gamma <- (sum(obs)) / (sum(pop))
+  rate <- obs / pop
+  sum.pop <- sum(pop)
         
-        phi.left <- sum(pop*(rate - gamma)^2) / sum.pop
-        phi.right <- gamma / mean(pop)
-        phi <- phi.left - phi.right
+  phi.left <- sum(pop * (rate - gamma)^2) / sum.pop
+  phi.right <- gamma / mean(pop)
+  phi <- phi.left - phi.right
         
-        # The convention is that phi = 0 whenever the above expression is negative.
-        ifelse(phi < 0, 0, phi)
-            
-        emp <- ((phi * (rate - gamma)) / (phi + (gamma / pop))) + gamma
+  # The convention is that phi = 0 whenever the above expression is negative.
+  phi <- ifelse(phi < 0, 0, phi)
+  emp <- ((phi * (rate - gamma)) / (phi + (gamma / pop))) + gamma
         
-        # gamma = nu / alpha
-        # phi = nu / alpha^2
+  # gamma = nu / alpha
+  # phi = nu / alpha^2
         
-        alpha <- gamma / phi
-        nu <- gamma^2 / phi
-        inv.nu <- 1/nu
+  alpha <- gamma / phi
+  nu <- gamma^2 / phi
+  inv.nu <- 1 / nu
                 
-        out <- as.data.frame(cbind(gamma, phi, alpha, nu, inv.nu))
-        names(out) <- c("gamma (mean)", "phi (variance)", "alpha (shape)", "nu (scale)", "inv.nu (rate)")
-        unlist(out)
-    }
+  rval <- data.frame(gamma, phi, alpha, nu, inv.nu)
+  names(rval) <- c("gamma (mean)", "phi (variance)", "alpha (shape)", "nu (scale)", "inv.nu (rate)")
+  unlist(rval)
+}
     
     
