9ab443626480c059e309ea2f9d9d511e5202b805
Mark Stevenson <mark.stevenson1@unimelb.edu.au>
2017-12-11 11:08:21 +0000

    version 0.9-93

--- R/epi.cp.R
+++ R/epi.cp.R
@@ -1,7 +1,9 @@
 epi.cp <- function(dat){
   
-  # Re-write of function following email from Johann Popp 11 October 2017.
+  # Re-write of function following email from Johann Popp 11 October 2017. Function modified to handle one covariate (following email with Mathew Jay)
   ndat <- data.frame(id = 1:nrow(dat), dat)
+  
+  if (!is.null(dim(ndat[,ncol(ndat):2]))) {
   
   # Add an indicator variable for covariate patterns:
   ndat$indi <- apply(X = ndat[,ncol(ndat):2], MARGIN = 1, FUN = function(x) as.factor(paste(x, collapse = "")))
@@ -26,5 +28,16 @@ epi.cp <- function(dat){
   id <- lookup$id[match(ndat$indi, lookup$indi)]
   # id <- as.numeric(unlist(lapply(strsplit(cp.id, ","), function(x) rep(min(as.numeric(unlist(x))), length(x)))))[order(ndat$id)]
   
+  list(cov.pattern = cov.pattern, id = id)
+  }
+  
+  else {
+    # ndat <- ndat[order(ndat[2]),]
+    cp.id <- tapply(ndat$id, ndat[2], function(x) paste(x, collapse = ","))
+    cp <- unique(ndat[, 2:ncol(ndat)])
+    n <- as.numeric(unlist(lapply(strsplit(cp.id, ","), length)))
+    cov.pattern <- data.frame(id = 1:length(n), n, cp)
+    id <- cov.pattern$id[match(as.vector(as.matrix(ndat[2])), cov.pattern$cp)]
+  }
   list(cov.pattern = cov.pattern, id = id)
 }
--- R/epi.directadj.R
+++ R/epi.directadj.R
@@ -1,4 +1,4 @@
-"epi.directadj" <- function(obs, pop, std, units = 1, conf.level = 0.95){   
+"epi.directadj" <- function(obs, tar, std, units = 1, conf.level = 0.95){   
     # How many strata (rows) are there?
     n.strata <- dim(obs)[1]
         
@@ -10,56 +10,56 @@
     z <- qnorm(N., mean = 0, sd = 1)
     
     # Note inclusion of variable sindex (strata index) to make sure strata estimates are sorted in the right order:
-    tmp <- data.frame(sindex = rep(1:nrow(pop), times = n.cov), strata = rep(rownames(pop), times = n.cov), cov = rep(colnames(pop), each = n.strata), 
-      obs = as.vector(obs), pop = as.vector(pop), std = as.vector(std))
+    tmp <- data.frame(sindex = rep(1:nrow(tar), times = n.cov), strata = rep(rownames(tar), times = n.cov), cov = rep(colnames(tar), each = n.strata), 
+      obs = as.vector(obs), tar = as.vector(tar), std = as.vector(std))
     
     # Crude incidence risk by strata-covariate:
-    # tmp <- data.frame(tmp, (epi.conf(as.matrix(cbind(tmp$obs, tmp$pop)), ctype = "inc.risk", method = method, 
+    # tmp <- data.frame(tmp, (epi.conf(as.matrix(cbind(tmp$obs, tmp$tar)), ctype = "inc.risk", method = method, 
     #   design = 1, conf.level = conf.level) *  units))
     
     # Expected events (equals observed incidence risk multiplied by standard population size):
-    tmp$exp <- (tmp$obs / tmp$pop) * tmp$std    
+    tmp$exp <- (tmp$obs / tmp$tar) * tmp$std    
     
     # Crude (for all strata-covariate combinations):
     t.obs <- tmp$obs
-    t.pop <- tmp$pop
+    t.tar <- tmp$tar
     
     # Confidence interval for crude incidence risk estimates corrected following email from Gillian Raab:
-    crude.p <- t.obs / t.pop
-    # crude.se <- crude.p / sqrt(t.pop)                            ## Incorrect.
-    crude.se <- crude.p / sqrt(t.obs)                              ## replaced pop by obs
-    crude.l <- qchisq(alpha / 2, 2 * t.obs) / 2 / t.pop            ## next 2 lines changed
-    crude.u <- qchisq(1 - alpha / 2, 2 * (t.obs + 1)) / 2 / t.pop
+    crude.p <- t.obs / t.tar
+    # crude.se <- crude.p / sqrt(t.tar)                            ## Incorrect.
+    crude.se <- crude.p / sqrt(t.obs)                              ## replaced tar by obs
+    crude.l <- qchisq(alpha / 2, 2 * t.obs) / 2 / t.tar            ## next 2 lines changed
+    crude.u <- qchisq(1 - alpha / 2, 2 * (t.obs + 1)) / 2 / t.tar
     crude <- data.frame(strata = tmp$strata, cov = tmp$cov, est = as.numeric(crude.p * units), lower = as.numeric(crude.l * units), upper = as.numeric(crude.u * units))
     
     
     # Crude strata:
     t.obs <- by(data = tmp$obs, INDICES = tmp$sindex, FUN = sum)
-    t.pop <- by(data = tmp$pop, INDICES = tmp$sindex, FUN = sum)
-    t.strata <- rownames(pop)
+    t.tar <- by(data = tmp$tar, INDICES = tmp$sindex, FUN = sum)
+    t.strata <- rownames(tar)
     
     # Confidence interval for crude incidence risk estimates corrected following email from Gillian Raab:
-    crude.p <- as.numeric(t.obs / t.pop)
-    # crude.se <- crude.p / sqrt(t.pop)                             ## Incorrect.
+    crude.p <- as.numeric(t.obs / t.tar)
+    # crude.se <- crude.p / sqrt(t.tar)                             ## Incorrect.
     crude.se <- as.numeric(crude.p / sqrt(t.obs))                   ## replaced pop by obs
-    crude.l <- as.numeric(qchisq(alpha / 2, 2 * t.obs) / 2 / t.pop) ## next 2 lines changed
-    crude.u <- as.numeric(qchisq(1 - alpha / 2, 2 * (t.obs + 1)) / 2 / t.pop)
+    crude.l <- as.numeric(qchisq(alpha / 2, 2 * t.obs) / 2 / t.tar) ## next 2 lines changed
+    crude.u <- as.numeric(qchisq(1 - alpha / 2, 2 * (t.obs + 1)) / 2 / t.tar)
     crude.strata <- data.frame(strata = t.strata, est = as.numeric(crude.p * units), lower = as.numeric(crude.l * units), upper = as.numeric(crude.u * units))
 
     
     # Adjusted incidence risk, by strata. Confidence intervals based on Fay and Feuer (1997):
     t.obs <- by(data = tmp$obs, INDICES = tmp$sindex, FUN = sum)
-    t.pop <- by(data = tmp$pop, INDICES = tmp$sindex, FUN = sum)
+    t.tar <- by(data = tmp$tar, INDICES = tmp$sindex, FUN = sum)
     t.exp <- by(data = tmp$exp, INDICES = tmp$sindex, FUN = sum)
-    t.strata <- rownames(pop)
+    t.strata <- rownames(tar)
     
     t.std <- by(data = tmp$std, INDICES = tmp$sindex, FUN = sum)
     tstd <- matrix(rep(std, time = n.strata), byrow = TRUE, nrow = n.strata)
     stdwt <- tstd / apply(X = tstd, MARGIN = 1, FUN = sum)
     
-    adj.p <- apply(X = stdwt * (obs / pop), MARGIN = 1, FUN = sum)
-    adj.var <- apply((stdwt^2) * (obs / pop^2), MARGIN = 1, FUN = sum)
-    wm <- apply(stdwt / pop, MARGIN = 1, FUN = max)
+    adj.p <- apply(X = stdwt * (obs / tar), MARGIN = 1, FUN = sum)
+    adj.var <- apply((stdwt^2) * (obs / tar^2), MARGIN = 1, FUN = sum)
+    wm <- apply(stdwt / tar, MARGIN = 1, FUN = max)
     
     adj.l <- qgamma(alpha / 2, shape = (adj.p^2) / adj.var, scale = adj.var / adj.p)
     adj.u <- qgamma(1 - alpha/2, shape = ((adj.p + wm)^2) / (adj.var + wm^2), scale = (adj.var + wm^2) / (adj.p + wm))
